814f8cf1f77eb61f6d1e179a8b6eeda59db1a418
Costin98 <morun.costin@yahoo.com>
2023-01-31 12:28:26 +0200

    added links eng language
    limit news
    added subject
    control category

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import requests
 from send_email import sendEmail
 
+
+topic='apple'
 apiKey = '32ca73b20c254b22b027792b060d0dd8'
-url = "https://newsapi.org/v2/everything?q=apple&from=2023-01-30&to=2023-01-30&sortBy=popularity&apiKey=32ca73b20c254b22b027792b060d0dd8"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-01-30&to=2023-01-30&sortBy=popularity&apiKey=32ca73b20c254b22b027792b060d0dd8&language=en"
 
 req = requests.get(url)
 
 content = req.json()
 
 body = ''
-for article in content['articles']:
+for article in content['articles'][0:20]:
     if article['title'] is not None:
-
-        body = body + article['title'] + '\n' + article['description'] + 2 * '\n'
+        body ='Subject:News for Today'+'\n'+ body + article['title'] + '\n' + article['description'] + '\n' + article['url'] + 2 * '\n'
 
 body = body.encode('utf-8')
 sendEmail(message=body)
