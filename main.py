b8505fc78776c7fc4876034036aa1e24842c1ad6
Costin98 <morun.costin@yahoo.com>
2023-01-31 12:15:36 +0200

    send an email with news titles and desc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 import requests
+from send_email import sendEmail
 
 apiKey = '32ca73b20c254b22b027792b060d0dd8'
 url = "https://newsapi.org/v2/everything?q=apple&from=2023-01-30&to=2023-01-30&sortBy=popularity&apiKey=32ca73b20c254b22b027792b060d0dd8"
@@ -8,8 +8,11 @@ req = requests.get(url)
 
 content = req.json()
 
-
+body = ''
 for article in content['articles']:
+    if article['title'] is not None:
 
-    print(article['title'])
-    print(article['description'])
+        body = body + article['title'] + '\n' + article['description'] + 2 * '\n'
+
+body = body.encode('utf-8')
+sendEmail(message=body)
---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib
+import ssl
+import os
+# I have created a random Gmail account for this experiment
+# This is not my daily use Gmail address
+def sendEmail(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    username = 'dnero543@gmail.com'
+    password = os.getenv('password')
+    receive = 'dnero543@gmail.com'
+    context = ssl.create_default_context()
+
+
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receive, message)
